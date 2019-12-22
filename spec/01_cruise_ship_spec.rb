cb4b9481624f1f736521ede53030e88cb4128201
Alexis Priore <alexispriore@gmail.com>
2019-12-22 20:27:05 +0000

    Done.

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -11,7 +11,15 @@ describe "#select_winner" do
       suite_d: "Charlie Chaplin", 
       suite_e: "Crumpet the Elf"
       }
-
-    expect(select_winner(passengers)).to eq("Amanda Presley")
+#
+  expect(select_winner(passengers)).to eq("Amanda Presley")
+  winner = " "
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
   end
 end
